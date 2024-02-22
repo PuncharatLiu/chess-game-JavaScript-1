8fe1d2cb0b9cecd23387868afee63660ca961c40
PuncharatLiu <puncharat46gg@gmail.com>
2024-02-22 21:42:02 +0700

    replay 4 - longCastle

--- scripts/PGN.js
+++ scripts/PGN.js
@@ -1,53 +1,80 @@
 import { Is } from "./pieces.js";
 import { getPieceId } from "./piecesControl.js";
-import KingEvent from "./handleKingEvent.js"
+import KingEvent from "./handleKingEvent.js";
 
 class PGN {
   constructor(file, rank, overlapBlack, overlapWhite) {
     this.file = file;
     this.rank = rank;
-    this.pgnPair = [];    
+    this.pgnPair = [];
     this.kingEvent = new KingEvent(overlapBlack, overlapWhite);
   }
 
-
   pgn(event, fileBackUp) {
     const majorPiece = ["R", "N", "B", "Q", "K"];
-    const isMajor = majorPiece.includes(this.pieceNotation())
-    let pawnFile
-    
-    if (fileBackUp) {pawnFile = parseInt(fileBackUp[0])};
+    const isMajor = majorPiece.includes(this.pieceNotation());
+    let pawnFile;
 
-    switch(event){
+    if (fileBackUp) {
+      pawnFile = parseInt(fileBackUp[0]);
+    }
+
+    switch (event) {
       case "capture":
-        if (isMajor){
-          return this.pieceNotation() + "x" + this.pgnFile(this.file) + this.pgnRank(this.rank);
-        } else {          
-          return this.pgnFile(parseInt(pawnFile)) + "x" + this.pgnFile(this.file) + this.pgnRank(this.rank);
+        if (isMajor) {
+          return (
+            this.pieceNotation() +
+            "x" +
+            this.pgnFile(this.file) +
+            this.pgnRank(this.rank)
+          );
+        } else {
+          return (
+            this.pgnFile(parseInt(pawnFile)) +
+            "x" +
+            this.pgnFile(this.file) +
+            this.pgnRank(this.rank)
+          );
         }
       case "check":
-        if (isMajor){          
-          return this.pieceNotation() + this.pgnFile(this.file) + this.pgnRank(this.rank) + "+"
-        } else {          
+        if (isMajor) {
+          return (
+            this.pieceNotation() +
+            this.pgnFile(this.file) +
+            this.pgnRank(this.rank) +
+            "+"
+          );
+        } else {
           return this.pgnFile(this.file) + this.pgnRank(this.rank) + "+";
         }
-      case "captureWithCheck":        
-        if (isMajor){
-          return this.pieceNotation() + "x" + this.pgnFile(this.file) + this.pgnRank(this.rank) + "+";
-        } else {          
+      case "captureWithCheck":
+        if (isMajor) {
+          return (
+            this.pieceNotation() +
+            "x" +
+            this.pgnFile(this.file) +
+            this.pgnRank(this.rank) +
+            "+"
+          );
+        } else {
           return "x" + this.pgnFile(this.file) + this.pgnRank(this.rank) + "+";
         }
       case "shortCastle":
         return "O-O";
+      case "longCastle":
+        return "O-O-O";
       default:
-        if (isMajor){          
-          return this.pieceNotation() + this.pgnFile(this.file) + this.pgnRank(this.rank);   
-        } else {          
+        if (isMajor) {
+          return (
+            this.pieceNotation() +
+            this.pgnFile(this.file) +
+            this.pgnRank(this.rank)
+          );
+        } else {
           return this.pgnFile(this.file) + this.pgnRank(this.rank);
-        }   
-      }
+        }
+    }
   }
-
 
   pgnFile(file) {
     switch (file) {
@@ -56,15 +83,15 @@ class PGN {
       case 1:
         return "b";
       case 2:
-        return "c";        
+        return "c";
       case 3:
-        return "d";        
+        return "d";
       case 4:
-        return "e";        
+        return "e";
       case 5:
-        return "f";        
+        return "f";
       case 6:
-        return "g";        
+        return "g";
       case 7:
         return "h";
       default:
@@ -72,30 +99,28 @@ class PGN {
     }
   }
 
-
   pgnRank(rank) {
     switch (rank) {
       case 0:
         return "8";
       case 1:
-        return "7";      
+        return "7";
       case 2:
-        return "6";     
+        return "6";
       case 3:
-        return "5";      
+        return "5";
       case 4:
-        return "4";      
+        return "4";
       case 5:
-        return "3";      
+        return "3";
       case 6:
-        return "2";      
+        return "2";
       case 7:
-        return "1";      
+        return "1";
       default:
         break;
     }
   }
-
 
   pieceNotation() {
     if (Is.rook(getPieceId)) {
@@ -111,7 +136,7 @@ class PGN {
     } else {
       return this.pgnFile(parseInt(this.fileBackUp));
     }
-  } 
+  }
 }
 
 export default PGN;
--- scripts/game-control-panel/move-replay.js
+++ scripts/game-control-panel/move-replay.js
@@ -6,7 +6,7 @@ import { overlapBlack, overlapWhite } from "../position.js";
 
 let pgnList = [];
 let pgnLeft, pgnRight, pgnPairIndex;
-let moveList = [[[],["start"]]];
+let moveList = [[[], ["start"]]];
 let currentMove = 0;
 let position;
 
@@ -15,7 +15,7 @@ const nextBtn = document.querySelector(".next-btn");
 prevBtn.addEventListener("click", previous);
 nextBtn.addEventListener("click", next);
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
   if (event.key === "ArrowLeft") {
     previous();
   } else if (event.key === "ArrowRight") {
@@ -23,11 +23,9 @@ document.addEventListener("keydown", function(event) {
   }
 });
 
-
-function createMoveList(move){
-  pgnList.push(move);  
+function createMoveList(move) {
+  pgnList.push(move);
 }
-
 
 function highlightMovePair(currentRemove, currentAdd) {
   const movePair = document.querySelectorAll(".move-pair");
@@ -36,73 +34,72 @@ function highlightMovePair(currentRemove, currentAdd) {
   movePair[currentAdd]?.classList.add("move-pair-highlight");
 }
 
-
-function displayPgnContent (move){        
+function displayPgnContent(move) {
   createMoveList(move);
-  
-  if (pgnList.length % 2 !== 0){
-    createReplayComponent();  
+
+  if (pgnList.length % 2 !== 0) {
+    createReplayComponent();
     pgnPairIndex.innerText = (pgnList.length + 1) / 2;
   }
-  pgnList.length % 2 !== 0 ? pgnLeft.innerText = move : pgnRight.innerText = move;    
-  
+  pgnList.length % 2 !== 0
+    ? (pgnLeft.innerText = move)
+    : (pgnRight.innerText = move);
+
   highlightMovePair(currentMove - 1, currentMove);
-  currentMove++;  
+  currentMove++;
 }
 
-
 function createReplayComponent() {
-  const pgnReplayWrap = document.querySelector(".move-replay-wrap");            // get move-replay-wrap element
-  const pgnPairWrap = document.createElement("div");                            // create move-pair-wrap
-  pgnLeft = document.createElement("div");                                      // create move-pair
-  pgnRight = document.createElement("div");                                     // create move-pair
-  pgnPairIndex = document.createElement("div");                                 // create move-pair-index
-  pgnPairWrap.classList.add("move-pair-wrap");                                  // add move-pair-wrap class to pgnPairWrap element
-  pgnLeft.classList.add("move-pair");                                           // add "move-pair" class to left pgn
-  pgnRight.classList.add("move-pair");                                          // add "move-pair" class to right pgn
-  pgnPairIndex.classList.add("move-pair-index");                                // add "move-pair-wrap" class to pgnPairWrap element.
+  const pgnReplayWrap = document.querySelector(".move-replay-wrap"); // get move-replay-wrap element
+  const pgnPairWrap = document.createElement("div"); // create move-pair-wrap
+  pgnLeft = document.createElement("div"); // create move-pair
+  pgnRight = document.createElement("div"); // create move-pair
+  pgnPairIndex = document.createElement("div"); // create move-pair-index
+  pgnPairWrap.classList.add("move-pair-wrap"); // add move-pair-wrap class to pgnPairWrap element
+  pgnLeft.classList.add("move-pair"); // add "move-pair" class to left pgn
+  pgnRight.classList.add("move-pair"); // add "move-pair" class to right pgn
+  pgnPairIndex.classList.add("move-pair-index"); // add "move-pair-wrap" class to pgnPairWrap element.
   pgnReplayWrap.appendChild(pgnPairWrap);
   pgnPairWrap.appendChild(pgnPairIndex);
   pgnPairWrap.appendChild(pgnLeft);
   pgnPairWrap.appendChild(pgnRight);
 }
 
-
-function getPisition(fromPosition, toPosition, event, CAPTURE){
-  position = [[],[]]
+function getPisition(fromPosition, toPosition, event, CAPTURE) {
+  position = [[], []];
   position[0].push(fromPosition, toPosition);
   position[1].push(event);
   position[1].push(CAPTURE);
-  moveList.push(position);  
+  moveList.push(position);
   console.log(moveList);
 }
 
-
-function previous() { replay("PREV"); }                 
-function next() { replay("NEXT"); }
-
-
-function replay(replayType){     
-  if (replayType === "PREV" && currentMove !== 0) {
-    const getEvent = moveList[currentMove][1][0];    
-    changePosition(replayType, getEvent);    
-    currentMove--;    
-  } 
-  else if (replayType === "NEXT" && currentMove + 1 !== moveList.length) {
-    currentMove++;
-    const getEvent = moveList[currentMove][1][0];       
-    changePosition(replayType, getEvent);
-  } 
+function previous() {
+  replay("PREV");
+}
+function next() {
+  replay("NEXT");
 }
 
+function replay(replayType) {
+  if (replayType === "PREV" && currentMove !== 0) {
+    const getEvent = moveList[currentMove][1][0];
+    changePosition(replayType, getEvent);
+    currentMove--;
+  } else if (replayType === "NEXT" && currentMove + 1 !== moveList.length) {
+    currentMove++;
+    const getEvent = moveList[currentMove][1][0];
+    changePosition(replayType, getEvent);
+  }
+}
 
-function changePosition(replayType, event){
-  if (replayType === "PREV") {    
-    highlightMovePair(currentMove - 1, currentMove - 2);    
-    
+function changePosition(replayType, event) {
+  if (replayType === "PREV") {
+    highlightMovePair(currentMove - 1, currentMove - 2);
+
     const getMoveInfo = [...moveList[currentMove]];
     if (event === "shortCastle") {
-      if (getMoveInfo[1][1] === "white") {        
+      if (getMoveInfo[1][1] === "white") {
         const kingElement = document.getElementById(getMoveInfo[0][0]);
         const rookElement = document.getElementById(getMoveInfo[0][1]);
 
@@ -113,9 +110,8 @@ function changePosition(replayType, event){
         pieces[31].position.file = 7;
         pieces[31].position.rank = 7;
         kingElement.setAttribute("position", "47");
-        rookElement.setAttribute("position", "77");        
-      } 
-      else {
+        rookElement.setAttribute("position", "77");
+      } else {
         const kingElement = document.getElementById(getMoveInfo[0][0]);
         const rookElement = document.getElementById(getMoveInfo[0][1]);
 
@@ -126,40 +122,70 @@ function changePosition(replayType, event){
         pieces[7].position.file = 7;
         pieces[7].position.rank = 0;
         kingElement.setAttribute("position", "40");
-        rookElement.setAttribute("position", "70");        
+        rookElement.setAttribute("position", "70");
       }
 
       SWITCH_TURN(turn);
       getCurrentPosition();
-      calculateAttackSquare();          
+      calculateAttackSquare();
+      return;
+    } else if (event === "longCastle") {
+      if (getMoveInfo[1][1] === "white") {
+        const kingElement = document.getElementById(getMoveInfo[0][0]);
+        const rookElement = document.getElementById(getMoveInfo[0][1]);
+
+        kingElement.style.transform = `translate(${400}px,${700}px)`;
+        rookElement.style.transform = `translate(${0}px,${700}px)`;
+        pieces[28].position.file = 4;
+        pieces[28].position.rank = 7;
+        pieces[24].position.file = 0;
+        pieces[24].position.rank = 7;
+        kingElement.setAttribute("position", "47");
+        rookElement.setAttribute("position", "07");
+      } else {
+        const kingElement = document.getElementById(getMoveInfo[0][0]);
+        const rookElement = document.getElementById(getMoveInfo[0][1]);
+
+        kingElement.style.transform = `translate(${400}px,${0}px)`;
+        rookElement.style.transform = `translate(${0}px,${0}px)`;
+        pieces[4].position.file = 4;
+        pieces[4].position.rank = 0;
+        pieces[0].position.file = 0;
+        pieces[0].position.rank = 0;
+        kingElement.setAttribute("position", "40");
+        rookElement.setAttribute("position", "00");
+      }
+
+      SWITCH_TURN(turn);
+      getCurrentPosition();
+      calculateAttackSquare();
       return;
     }
 
     const piece = document.querySelector(`[position="${getMoveInfo[0][1]}"]`);
-    const pieceId = piece.id;  
+    const pieceId = piece.id;
     const toPosition = getMoveInfo[0][0];
     const toFile = parseInt(toPosition[0]);
     const toRank = parseInt(toPosition[1]);
-    
+
     piece.style.transform = `translate(${toFile * 100}px, ${toRank * 100}px)`;
     pieces[parseInt(pieceId)].position.file = toFile;
     pieces[parseInt(pieceId)].position.rank = toRank;
     piece.setAttribute("position", toPosition[0] + toPosition[1]);
-    
-    if (event === "capture") {      
-      const capturedPiece = getMoveInfo[1][1]; 
-      const capturedPieceId = capturedPiece.id;      
-      const toPosition = getMoveInfo[0][1]
-      
+
+    if (event === "capture") {
+      const capturedPiece = getMoveInfo[1][1];
+      const capturedPieceId = capturedPiece.id;
+      const toPosition = getMoveInfo[0][1];
+
       capturedPiece.style.transform = `translate(${parseInt(getMoveInfo[0][1][0]) * 100}px, ${parseInt(getMoveInfo[0][1][1]) * 100}px)`;
       pieces[parseInt(capturedPieceId)].position.file = parseInt(toPosition[1]);
       pieces[parseInt(capturedPieceId)].position.rank = parseInt(toPosition[2]);
       capturedPiece.setAttribute("position", toPosition[0] + toPosition[1]);
-    } 
-
-  } else if (replayType === "NEXT"){    
+    }
+  } else if (replayType === "NEXT") {
     highlightMovePair(currentMove - 2, currentMove - 1);
-    
+
     const getMoveInfo = [...moveList[currentMove]];
     if (event === "shortCastle") {
       if (getMoveInfo[1][1] === "white") {
@@ -173,9 +199,8 @@ function changePosition(replayType, event){
         pieces[31].position.file = 5;
         pieces[31].position.rank = 7;
         kingElement.setAttribute("position", "67");
-        rookElement.setAttribute("position", "57");        
-      } 
-      else {
+        rookElement.setAttribute("position", "57");
+      } else {
         const kingElement = document.getElementById(getMoveInfo[0][0]);
         const rookElement = document.getElementById(getMoveInfo[0][1]);
 
@@ -186,12 +211,43 @@ function changePosition(replayType, event){
         pieces[7].position.file = 5;
         pieces[7].position.rank = 0;
         kingElement.setAttribute("position", "60");
-        rookElement.setAttribute("position", "50");        
+        rookElement.setAttribute("position", "50");
       }
 
       SWITCH_TURN(turn);
       getCurrentPosition();
-      calculateAttackSquare();          
+      calculateAttackSquare();
+      return;
+    } else if (event === "longCastle") {
+      if (getMoveInfo[1][1] === "white") {
+        const kingElement = document.getElementById(getMoveInfo[0][0]);
+        const rookElement = document.getElementById(getMoveInfo[0][1]);
+
+        kingElement.style.transform = `translate(${200}px,${700}px)`;
+        rookElement.style.transform = `translate(${300}px,${700}px)`;
+        pieces[28].position.file = 2;
+        pieces[28].position.rank = 7;
+        pieces[24].position.file = 3;
+        pieces[24].position.rank = 7;
+        kingElement.setAttribute("position", "27");
+        rookElement.setAttribute("position", "37");
+      } else {
+        const kingElement = document.getElementById(getMoveInfo[0][0]);
+        const rookElement = document.getElementById(getMoveInfo[0][1]);
+
+        kingElement.style.transform = `translate(${200}px,${0}px)`;
+        rookElement.style.transform = `translate(${300}px,${0}px)`;
+        pieces[4].position.file = 2;
+        pieces[4].position.rank = 0;
+        pieces[0].position.file = 3;
+        pieces[0].position.rank = 0;
+        kingElement.setAttribute("position", "20");
+        rookElement.setAttribute("position", "30");
+      }
+
+      SWITCH_TURN(turn);
+      getCurrentPosition();
+      calculateAttackSquare();
       return;
     }
 
@@ -207,26 +263,29 @@ function changePosition(replayType, event){
     piece.setAttribute("position", toPosition[0] + toPosition[1]);
 
     if (event === "capture") {
-      const capturedPiece = getMoveInfo[1][1];            
-      const capturedPieceId = capturedPiece.id;      
-      const toPosition = getMoveInfo[0][1]
-      
+      const capturedPiece = getMoveInfo[1][1];
+      const capturedPieceId = capturedPiece.id;
+      const toPosition = getMoveInfo[0][1];
+
       capturedPiece.style.transform = `translate(${-1000}px, ${-1000}px)`;
       pieces[parseInt(capturedPieceId)].position.file = -1000;
-      pieces[parseInt(capturedPieceId)].position.rank = -1000;      
-      capturedPiece.setAttribute("position", `-${toPosition[0]}${toPosition[1]}`);      
+      pieces[parseInt(capturedPieceId)].position.rank = -1000;
+      capturedPiece.setAttribute(
+        "position",
+        `-${toPosition[0]}${toPosition[1]}`,
+      );
     }
   }
 
   SWITCH_TURN(turn);
   getCurrentPosition();
-  calculateAttackSquare();    
+  calculateAttackSquare();
 }
-
 
 const Replay = {
   displayPgnContent: displayPgnContent,
-  getPosition: getPisition
-}
+  getPosition: getPisition,
+};
 
 export default Replay;
+
